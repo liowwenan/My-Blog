6901b3fd3dd1aab92e5b660e332bbf52d6d4e2ad
liowwenan <79820566+liowwenan@users.noreply.github.com>
2023-03-19 11:51:59 +0800

    Fetch Data from users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //Show List of users
+    function index()
+    {
+        //Process
+        $users = User::get();//Select * FROM users
+        return view ('users.index', compact('users'));
+    }
 }
